2c06193ecedbb95d51cbebdd8dcde52d29aea557
AJ Wu <ajwu@AJs-MacBook-Pro.lw.hs>
2018-05-15 09:36:02 -0700

    deleted the extra city and left only the one that corresponds with w,z

--- Game/sketch.js
+++ Game/sketch.js
@@ -112,57 +112,7 @@ class sprite {
    }
 }
 
-
-function city(w,z){//change lines into houses
-  strokeWeight(30);
-  stroke('black')
-  //top
-  line(w-100,z-100,w+900,z-100);//top side line
-  line(w+900,z-100,w+900,z+900);//right side line
-  line(w-100,z,w+100,z);
-  line(w+200,z-100,w+200,z);
-  line(w+200,z,w+450,z);
-  line(w+570,z-100,w+570,z);
-  line(w+570,z,w+800,z);
-  line(w,z+100,w+600,z+100);
-  line(w+600,z+100,w+600,z+250);
-  line(w+700,z+100,w+700,z+250);
-  line(w+700,z+250,w+800,z+250);
-  line(w+800,z+250,w+800,z+500);
-  line(w+800,z+500,w+700,z+500);
-//part C
-  line(w+800,z+100,w+800,z+170);
-  line(w+800,z+170,w+900,z+170);
-  line(w-100,z+200,w+100,z+200);
-  line(w-100,z-100,w-100,z+900);//left side line
-  line(w+100,z+200,w+100,z+350);
-  line(w+170,z+350,w,z+350);
-  line(w,z+350,w,z+290);
-  line(w+170,z+450,w,z+450);
-//part B
-  line(500,200,500,450);
-  line(500,450,800,450);
-  line(800,450,800,1000);
-  line(300,300,400,300);
-  line(400,300,400,550);
-  line(400,550,700,550);
-  line(700,550,700,750);
-  //part A
-  line(0,650,450,650);
-  line(400,650,400,800);
-  line(700,750,600,750);
-  line(600,750,600,1000);
-  line(700,860,700,1000);
-  line(1000,700,900,700);
-  line(800,800,900,800);
-  line(1000,1000,800,1000);
-  line(0,1000,600,1000);
-  line(500,800,500,1000);
-  line(300,800,300,1000);
-  line(300,800,100,800);
-}
-
-function city2(w,z){//w= 100, z= 100
+function city(w,z){//w= 100, z= 100
   strokeWeight(30);
   stroke('black')
   //top
